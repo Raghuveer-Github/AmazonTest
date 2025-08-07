e31ac77ebd79e9259348b55e99e86ef110a940b5
Raghuveer-Github <raghu.soni1993@gmail.com>
2025-08-07 19:11:41 +0530

    Update DashboardFeatures.java

--- AmazonDashboard/DashboardFeatures.java
+++ AmazonDashboard/DashboardFeatures.java
@@ -1,9 +1,16 @@
 package AmazonDashboard;
 
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import GenericLibrary.BaseClass;
@@ -90,5 +97,6 @@ public class DashboardFeatures extends BaseClass {
 	}
 
 }
+
 
 
